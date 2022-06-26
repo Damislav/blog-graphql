7e42a1b77790d25a610e69965ea8ccf6c1886b2c
yournewgoodname <damislav@net.hr>
2022-06-26 02:03:32 +0200

    first commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = module.exports = {
+  nextConfig,
+  images: {
+    domains: ["media.graphassets.com"],
+  },
+};
